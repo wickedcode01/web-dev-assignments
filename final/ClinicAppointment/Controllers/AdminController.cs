1f510a4178511949ea6a18fb3dca4341747b640b
yashvi22 <yashvihp22@gmail.com>
2025-03-27 23:53:24 -0400

    added functionality to edit appointment details on admin side

--- final/ClinicAppointment/Controllers/AdminController.cs
+++ final/ClinicAppointment/Controllers/AdminController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ClinicAppointment.Data;
+using ClinicAppointment.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Linq;
 
 namespace ClinicAppointment.Controllers
 {
-
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
@@ -19,6 +20,45 @@ namespace ClinicAppointment.Controllers
         {
             var appointments = _context.Appointments.OrderBy(a => a.AppointmentDate).ToList();
             return View(appointments);
+        }
+
+        // GET: Admin/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var appointment = _context.Appointments.FirstOrDefault(a => a.Id == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("Id, PatientEmail, Phone, AppointmentDate, Status")] Appointment appointment)
+        {
+            if (id != appointment.Id)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var existingAppointment = _context.Appointments.FirstOrDefault(a => a.Id == id);
+                if (existingAppointment == null)
+                {
+                    return NotFound();
+                }
+
+                existingAppointment.PatientEmail = appointment.PatientEmail;
+                existingAppointment.Phone = appointment.Phone;
+                existingAppointment.AppointmentDate = appointment.AppointmentDate;
+                existingAppointment.Status = appointment.Status;
+
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Appointments));
+            }
+            return View(appointment);
         }
     }
 }
